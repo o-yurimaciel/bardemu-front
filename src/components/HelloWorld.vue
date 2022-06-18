abb3de41043a452a8757b9ca84a3dc4da1bd0174
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-06-18 14:17:45 -0300

    setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>Hello! {{products}}</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.getProductList()
+  },
+  methods: {
+    getProductList() {
+      axios.get('https://bardemu-backend.herokuapp.com/products').then((res) => {
+        this.products = res.data
+        console.log(res)
+      }).catch((e) => {
+        console.log(e.response)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
